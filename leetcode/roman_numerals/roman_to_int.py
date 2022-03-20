0d83cb2c09555db35a3670bda1e09ce1d3bbe1b6
Michael Duncan <msduncan82@gmail.com>
2022-03-20 13:27:26 -0600

    Fixed poetry setup

--- leetcode/roman_numerals/roman_to_int.py
+++ leetcode/roman_numerals/roman_to_int.py
@@ -2,7 +2,7 @@
 class BadRomanNumeralError:
     """Raised when there is a problem with an input roman numeral"""
 
-class Solution:
+class RomanToInt:
     roman_numeral_lookup = {
         'I': 1,
         'V': 5,
@@ -13,15 +13,15 @@ class Solution:
         'M': 1000
     }
     
-    def roman_to_int(self, roman_string: str) -> int:
+    def romanToInt(self, roman_string: str) -> int:
         """Convert roman numeral to integer"""
-        num_list = self.roman_to_num_list(roman_string)
-        signed_nums = self.add_signs(num_list)
+        num_list = self._roman_to_num_list(roman_string)
+        signed_nums = self._add_signs(num_list)
         total = sum(signed_nums)
         
         return total
 
-    def single_roman_numeral_to_num(self, roman_numeral: str) -> int:
+    def _single_roman_numeral_to_num(self, roman_numeral: str) -> int:
         """Convert a single roman numeral to an integer"""
         num = self.roman_numeral_lookup.get(roman_numeral)
 
@@ -32,20 +32,22 @@ class Solution:
 
         return num
 
-    def roman_to_num_list(self, roman_string: str) -> list[int]:
+    def _roman_to_num_list(self, roman_string: str) -> list[int]:
         """Convert a roman numeral string to a list of integers"""
-        num_list = [self.single_roman_numeral_to_num(rn) for rn in roman_string]
+        num_list = [self._single_roman_numeral_to_num(rn) for rn in roman_string]
 
         return num_list
 
-    def add_signs(num_list: list[int]) -> list[int]:
+    def _add_signs(num_list: list[int]) -> list[int]:
         """
         Multiply the all numbers in a list before the max by -1
         
         [1, 1, 5, 5, 1, 1] -> [-1, -1, 5, 5, 1, 1]
         """
+        # Find index of max
         max_num = max(num_list)
         max_idx = num_list.index(max_num)
 
+        # Multiply all numbers before max by -1
         signed_list = [n * -1 if i < max_idx else n for i, n in enumerate(num_list)]
         return signed_list
