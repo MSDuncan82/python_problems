6757b94bb0e836b013c0a0488c3d3cd589c72a07
Michael Duncan <msduncan82@gmail.com>
2022-03-20 13:10:57 -0600

    rewrite solution after accidental deletion :(

--- leetcode/roman_numerals/roman_to_int.py
+++ leetcode/roman_numerals/roman_to_int.py
@@ -1,3 +1,7 @@
+
+class BadRomanNumeralError:
+    """Raised when there is a problem with an input roman numeral"""
+
 class Solution:
     roman_numeral_lookup = {
         'I': 1,
@@ -9,10 +13,39 @@ class Solution:
         'M': 1000
     }
     
-    def romanToInt(self, s: str) -> int:
-        num_list = self.s_to_list(s)
+    def roman_to_int(self, roman_string: str) -> int:
+        """Convert roman numeral to integer"""
+        num_list = self.roman_to_num_list(roman_string)
         signed_nums = self.add_signs(num_list)
         total = sum(signed_nums)
         
         return total
+
+    def single_roman_numeral_to_num(self, roman_numeral: str) -> int:
+        """Convert a single roman numeral to an integer"""
+        num = self.roman_numeral_lookup.get(roman_numeral)
+
+        if num is None:
+            raise BadRomanNumeralError(f"""
+            The roman numeral '{roman_numeral}' was not found. Please use one of: {list(self.roman_numeral_lookup.keys())}
+            """)
+
+        return num
+
+    def roman_to_num_list(self, roman_string: str) -> list[int]:
+        """Convert a roman numeral string to a list of integers"""
+        num_list = [self.single_roman_numeral_to_num(rn) for rn in roman_string]
+
+        return num_list
+
+    def add_signs(num_list: list[int]) -> list[int]:
+        """
+        Multiply the all numbers in a list before the max by -1
         
+        [1, 1, 5, 5, 1, 1] -> [-1, -1, 5, 5, 1, 1]
+        """
+        max_num = max(num_list)
+        max_idx = num_list.index(max_num)
+
+        signed_list = [n * -1 if i < max_idx else n for i, n in enumerate(num_list)]
+        return signed_list
